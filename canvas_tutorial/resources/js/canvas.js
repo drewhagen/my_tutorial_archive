467756726e9f31f0c33a2823cb5ec768d6487c2b
Drew Hagen <drew.a.hagen@gmail.com>
2017-07-22 23:58:43 -0500

    Drew a couple big boxes

--- canvas_tutorial/resources/js/canvas.js
+++ canvas_tutorial/resources/js/canvas.js
@@ -5,3 +5,10 @@ var canvas = document.querySelector('canvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+/* c.fillRect(x, y, width, height); */
+c.fillRect(100, 100, 100, 100);
+c.fillRect(400, 100, 100, 100);
+c.fillRect(300, 300, 100, 100);
