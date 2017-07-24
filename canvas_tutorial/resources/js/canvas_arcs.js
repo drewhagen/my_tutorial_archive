4662a16b0e76c71106cb97b02df5047520d26c3c
Drew Hagen <drew.a.hagen@gmail.com>
2017-07-23 23:08:31 -0500

    Playing with arcs, generate 100 circles on the page

---
+++ canvas_tutorial/resources/js/canvas_arcs.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Drew on 7/23/17.
+ *
+ * The tutorial for arcs starts here:
+ *      https://youtu.be/83L6B13ixQ0?t=7m7s
+ *
+ * REVIEW: how RADIANS work!
+ *  https://www.khanacademy.org/math/algebra2/
+ *  trig-functions/intro-to-radians-alg2/v/introduction-to-radians
+ *
+ */
+
