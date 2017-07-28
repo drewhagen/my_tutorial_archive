acc960739b14f78c113b86a85d05bd549a89d87f
Drew Hagen <drew.a.hagen@gmail.com>
2017-07-28 00:00:19 -0500

    bouncing circle!!!

--- canvas_tutorial/resources/js/canvas_animations.js
+++ canvas_tutorial/resources/js/canvas_animations.js
@@ -12,18 +12,31 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-var x = 200;
+var x = Math.random() * innerWidth;
+var y = Math.random() * innerHeight;
+var dx = (Math.random() - 0.5) * 4;
+var dy = (Math.random() - 0.5) * 4;
+var radius = 30;
 
 function animate() {
     requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
 
     //Let's start with a circle.
     c.beginPath();
-    c.arc(x, 200, 30, 0, Math.PI * 2, false);
+    c.arc(x, y, radius, 0, Math.PI * 2, false);
     c.strokeStyle = 'blue';
     c.stroke();
 
-    x+= 1;
+    if(x+radius > innerWidth || x-radius < 0) {
+        dx = -dx;
+    }
+    if(y+radius > innerHeight || y-radius < 0) {
+        dy = -dy;
+    }
+
+    x+= dx;
+    y+= dy;
 }
 
 animate();
